ce8036523a9c77af301b62a4e920c6da2f5b9bfb
BrotherSilas <silasedetsnr@gmail.com>
2024-08-16 03:59:23 -0700

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes the Apache 500 error by using sed to replace all instances of 'phpp' with 'php'
 
-exec { 'fix-wordpress':
+exec { 'fix-wordpress error':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
