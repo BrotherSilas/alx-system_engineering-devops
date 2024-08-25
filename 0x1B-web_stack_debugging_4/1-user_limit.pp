201daa811930e306388cff603dda842ee8723907
Silas Edet <silasedetsnr@gmail.com>
2024-08-25 20:21:26 +0000

    debugging4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,11 +2,11 @@
 # Changes os configuration for holberton userexec { 'change-os-configuration-for-holberton-user':
 
 exec { 'change-os-configuration-for-holberton-user':
-  command => "sed -i '/holberton hard/d' /etc/security/limits.conf &&
+  command  => "sed -i '/holberton hard/d' /etc/security/limits.conf &&
               sed -i '/holberton soft/d' /etc/security/limits.conf &&
               echo 'holberton hard nofile 8192' >> /etc/security/limits.conf &&
               echo 'holberton soft nofile 4096' >> /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/',
+  path     => '/usr/local/bin/:/bin/',
   provider => 'shell',
 }
 
